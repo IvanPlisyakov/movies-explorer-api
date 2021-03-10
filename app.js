65d0e966d7fe98c9a4a5e13698a0a93e3ae569cf
Ivan Pli <plisyakov.ivan@mail.ru>
2021-03-10 19:04:45 +0700

    13

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const { errors, isCelebrate } = require('celebrate');
+const { errors } = require('celebrate');
 const cors = require('cors');
 const routers = require('./routes/index.js');
 
@@ -11,7 +11,7 @@ const { centarlErrors } = require('./middlewares/central-errors');
 const NotFoundError = require('./errors/not-found-err');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect(process.env.DATABASE_URL, {
@@ -30,14 +30,6 @@ app.use('/', routers);
 app.use(() => {
   throw new NotFoundError('Запрашиваемый ресурс не найден');
 });
-/* app.use((err, req, res, next) => {
-  if (err.details) {
-    console.log(err.details.get('body').details[0].path);
-    return res.status(400).send({ messege: 'Ошибка валидации', keys: err.details.get('body').details[0].path});
-  }
-
-  return next(err);
-}); */
 app.use(errorLogger);
 app.use(errors());
 app.use(centarlErrors);
